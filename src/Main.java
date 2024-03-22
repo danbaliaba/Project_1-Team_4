31fb48b33a2d1e6b6fe6e8f4030089d04a010339
danbaliaba <danbaliab@gmail.com>
2024-03-22 12:48:06 +0100

    Latest Commit

--- src/Main.java
+++ src/Main.java
@@ -11,74 +11,13 @@ public class Main {
         Иначе оно требует все методы, вызываемые из класса CLIView сделать статическими.
         Так что пока это костыль
          */
-        CLIView CLIView = new CLIView();
+     //   CLIView CLIView = new CLIView(); // теперь вызов программы происходит через TicketRepository
 
         // VAL: Этот код ещё дописывается
 //        String ticketCSVList = String.valueOf(args[0]); // VAL: преобразую аргументу в строку и использую это в кач-ве аргументов метода
 //        CLIView.addTicketFromCVS(ticketCSVList); // VAL: вызываю метод, который берёт аргументы при запуске в кач-ве указания адреса CSV файла
 
-
-        System.out.println("Команда help выведет доступный список команд!");
-
-        String vvod;
-        do {
-            System.out.print("Введите команду: ");
-            vvod = insert.nextLine();
-
-            switch (vvod) {
-                case "help":
-                    System.out.println("info : вывести информацию о коллекции\n" +
-                            "show : вывести все элементы коллекции\n" +
-                            "add : добавить новый элемент в коллекцию\n" +
-                            "update {id} : обновить значение элемента коллекции\n" +
-                            "remove_by_id {id} : удалить элемент из коллекции по id\n" +
-                            "clear : очистить коллекцию\n" +
-                            "remove_at {index} : удалить элемент по индексу\n" +
-                            "remove_first : удалить первый элемент\n" +
-                            "filter_greater_than_type {type} : вывести элементы, значение поля type которых больше заданного\n" +
-                            "print_field_descending_person : вывести значения поля person всех элементов в порядке убывания");
-                    break;
-                    /* VAL: я починила вызов методов, раньше использовалось ticketManager.имя_метода,
-                    что устарело, т.к. теперь команды хранятся в CLIView */
-                case "info":
-                    CLIView.showInfo();
-                    break;
-                case "show":
-                    CLIView.showList();
-                    break;
-                case "add":
-                    CLIView.add();
-                    break;
-                case "update":
-                    CLIView.updateById();
-                    break;
-                case "remove_by_id":
-                    CLIView.removeById();
-                    break;
-                case "clear":
-                    CLIView.clearList();
-                    break;
-                case "remove_at":
-                    CLIView.removeAtIndex();
-                    break;
-                case "remove_first":
-                    CLIView.removeFirst();
-                    break;
-                case "filter_greater_than_type":
-                    CLIView.listSortType();
-                    break;
-                case "print_field_descending_person":
-//                    CLIView.personSorter(); VAL: пока что так, потому что метод не прописан, потом можно его "открыть"
-                    break;
-                case "exit":
-                    System.out.println("Завершение программы.");
-                    break;
-                default:
-                    System.out.println("Некорректная команда. Введите 'help' для списка команд.");
-                    break;
-            }
-        } while (!vvod.equals("exit"));
-
-        insert.close();
+        Controllers.TicketController ticketController = new Controllers.TicketController();
+        ticketController.start();
     }
 }
